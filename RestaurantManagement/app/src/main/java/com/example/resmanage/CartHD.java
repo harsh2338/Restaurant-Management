37c7f76d36771ee1af61b763bcb26a8e08c3bd47
harsh2338 <harsh.aayush.r1@gmail.com>
2019-04-27 22:14:34 +0530

    reformatted

--- RestaurantManagement/app/src/main/java/com/example/resmanage/CartHD.java
+++ RestaurantManagement/app/src/main/java/com/example/resmanage/CartHD.java
@@ -26,31 +26,22 @@ public class CartHD extends AppCompatActivity {
     private static final String TAG = "CartHD";
     public static float globaltotal;
     public static String globaladdress;
-
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     TextView xtotal;
     Button checkout;
-
-
     List<Order> cart = new ArrayList<>();
     CartAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
-
         xtotal = findViewById(R.id.total);
         recyclerView = findViewById(R.id.listCart);
         recyclerView.setHasFixedSize(true);
-
-
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-
         final String temp = NaviDraw.globalDineorHD;//intentForHD.getStringExtra("DineorHD");
-
         checkout = findViewById(R.id.checkout);
         checkout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,53 +51,41 @@ public class CartHD extends AppCompatActivity {
                 } else {
                     startActivity(new Intent(CartHD.this, Bill.class));
                 }
-
             }
         });
         loadListFood();
     }
-
     private void showAlertDialog() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(CartHD.this);
         alertDialog.setTitle("One more step!");
         alertDialog.setMessage("Enter your address");
-
         final EditText edtAddress = new EditText(CartHD.this);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
         );
         edtAddress.setLayoutParams(lp);
-
         alertDialog.setView(edtAddress);
         alertDialog.setIcon(R.drawable.ic_add_shopping_cart_black_24dp);
-
-
         alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 globaladdress = edtAddress.getText().toString();
                 startActivity(new Intent(CartHD.this, Bill.class));
-
             }
         });
-
         alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
             }
         });
-
         alertDialog.show();
-
     }
-
     private void loadListFood() {
         cart = new Database(this).getCarts();
         adapter = new CartAdapter(cart, this);
         recyclerView.setAdapter(adapter);
-
         globaltotal = 0;
         for (Order order : cart) {
 
@@ -115,9 +94,6 @@ public class CartHD extends AppCompatActivity {
         }
         Locale locale = new Locale("en", "IN");
         NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
-
         xtotal.setText(fmt.format(globaltotal));
-
-
     }
 }
